38d780888aabf804eb452c438dc67d03da59b158
Jelena Zujovic <jelazj@gmail.com>
2025-08-06 12:05:10 +0100

    Added Js to rotate slides - 5th commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
+   const totalSlides = $('.slide').length;
+   let currentSlide = 0;
+
+   $('.slide:first-child').addClass('active');
+
+   function rotateSlider() {
+    $('.slide').removeClass('active');
+    currentSlide = (currentSlide + 1) % totalSlides;
+    $('.slide').eq(currentSlide).addClass('active');
    
+  }
+  setInterval(rotateSlider, 4000);
+
 });
+
+
